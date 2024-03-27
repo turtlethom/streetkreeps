c62f89680cbade33135ccae2e8a978f860fa7277
Thomas James <wjamesthomas3@gmail.com>
2024-03-27 10:43:32 -0400

    Social Icon Changes

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -8,9 +8,24 @@ function Socials() {
         <div className="bg-black">
             <h1 className="fs-700">Follow Us On Social Media!</h1>
             <div className={`flex ${styles.socialIcons}`}>
-                <a href="#/facebook"><RiFacebookCircleFill size={socialSize}/></a>
-                <a href="#/snapchat"><RiSnapchatFill size={socialSize}/></a>
-                <a href="#/discord"><RiInstagramFill size={socialSize}/></a>
+                <a href="#/facebook">
+                    <RiFacebookCircleFill 
+                        size={socialSize} 
+                        color={'white'}
+                    />
+                </a>
+                <a href="#/snapchat">
+                    <RiSnapchatFill 
+                        size={socialSize}
+                        color={'white'}
+                    />
+                </a>
+                <a href="#/instagram">
+                    <RiInstagramFill 
+                        size={socialSize} 
+                        color={'white'}
+                    />
+                </a>
             </div>
         </div>
     );
