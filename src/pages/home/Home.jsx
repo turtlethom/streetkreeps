ab48db74e84e20ac14ff6eaae76535f7732775a0
Thomas James <wjamesthomas3@gmail.com>
2024-04-15 15:16:20 -0400

    Removed legacy code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import EmailSignup from "../../components/EmailSignup/EmailSignup";
 import HeroSlider from "../../components/HeroSlider/HeroSlider";
 import Socials from "../../components/Socials/Socials";
-import TestButton from "../../components/TestButton/TestButton";
+// import TestButton from "../../components/TestButton/TestButton";
 // import SUPABASE from "../../config/supabaseClient";
 
 export default function Home() {
