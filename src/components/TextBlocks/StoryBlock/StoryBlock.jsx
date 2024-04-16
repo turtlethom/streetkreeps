3c65d67cbde8ebc5eb82e047405c8b6e565cebd2
Thomas James <wjamesthomas3@gmail.com>
2024-04-16 13:17:50 -0400

    Slight spacing of syntax

--- src/components/TextBlocks/StoryBlock/StoryBlock.jsx
+++ src/components/TextBlocks/StoryBlock/StoryBlock.jsx
@@ -6,14 +6,14 @@ export default function StoryBlock() {
         <div className={styles.blockContainer}>
             <div className={styles.descriptionContainer}>
                 <h1>Our Community</h1>
-                <hr style={{inlineSize: '80%'}}/>
+                <hr style={{ inlineSize: '80%' }}/>
                 <p>
                 <span>What Sets Us Apart.</span>
                     What sets StreetKreeps apart is our unwavering commitment to authenticity and inclusivity. We embrace diversity in all its forms
                     and welcome enthusiasts from every background and perspective. Whether you're into classic muscle cars, JDM imports,
                     European exotics, or anything in between, you'll find like-minded individuals who share your passion within our community.
                 </p>
-                <hr style={{inlineSize: '80%'}}/>
+                <hr style={{ inlineSize: '80%' }}/>
             </div>
         </div>
     )
